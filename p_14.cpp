4720ffac3edb2740c4e156951779af69fdb24537
blindcoalminer <balog.andras.laszlo@gmail.com>
2025-10-01 21:48:15 +0200

    practice 14. [előkészített] - Írjunk egy programot, amely egy bekért számról megmondja hány valódi osztója van!

---
+++ p_14.cpp
@@ -0,0 +1,15 @@
+//
+// Created by blindcoalminer on 2025. 10. 01..
+// Írjunk egy programot, amely egy bekért számról megmondja hány valódi osztója van!
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // body
+
+    cout << endl << "Press any key to exit";
+    cin.ignore().get();
+    return 0;
+}
